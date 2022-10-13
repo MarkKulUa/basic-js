6c80242f91ed8dc2c2edb0872185411618813610
Mark Kulishov <>
2022-10-13 21:48:49 +0300

    dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
+
 
 /**
  * Determine the age of archeological find by using
@@ -17,11 +16,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function dateSample(sampleActivity) {
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+  let paramVal = parseFloat(sampleActivity);
 
+  if (typeof sampleActivity !== 'string' || !sampleActivity || !paramVal || paramVal < 0  || paramVal > 15) {
+    return false;
+  }
+
+  return  Math.ceil(Math.log(MODERN_ACTIVITY / paramVal) / ( 0.693 / HALF_LIFE_PERIOD));
+}
+console.log(dateSample('3'))
 module.exports = {
   dateSample
 };
